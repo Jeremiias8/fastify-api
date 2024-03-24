b8ceb246b498450bee0c9c7a6712808fe42e4aca
Jeremías <jeremiias122002@gmail.com>
2024-03-24 19:50:58 +0100

    second commit - hook y hookProvider

--- hooks/onReady.js
+++ hooks/onReady.js
@@ -1,7 +1,53 @@
 
 const fastify = require('fastify');
-// const provider = require('./provider');
+const provider = require('./provider');
 
-const server = fastify({ logger: true });
-const USUAL_WAIT_TIME_MS = 5000;
+const servidor = fastify({ logger: true });
+const TIEMPO_ESPERA_EN_MS = 5000;
 
+servidor.get('/ping', (req, reply) => {
+
+    reply.send({ error: false, ready: req.server.magic !== null });
+});
+
+servidor.post('/webhook', (req, reply) => {
+
+    const {magicKey} = req.body;
+    req.servidor.magicKey = magicKey;
+    req.log.info('Listo para las peticiones!');
+
+    reply.send({ error: false });
+});
+
+servidor.get('/v1*', async (req, reply) => {
+
+    try {
+        const data = await provider.fetchDataSensible(req.servidor.magicKey);
+        return { customer: true, error: false }
+
+    } catch (error) {
+
+        req.log.error({
+            error,
+            message: 'Fallaron las peticiones con la API del proveedor',
+        });
+    
+        reply.statusCode = 500;
+        return { customer: null, error: true }
+    }
+});
+
+servidor.decorate('magicKey', null);
+
+servidor.listen({ port: '3366' }, () => {
+
+    provider.thirdPartyMagicKeyGenerator(TIEMPO_ESPERA_EN_MS)
+        .catch((err) => {
+            servidor.log.error({
+                err,
+                message: 'Se obtuvo un error mientras se intentaba obtener la magicKey!'
+            });
+
+            servidor.close(() => process.exit(1))
+        });
+});
---
+++ hooks/provider.js
@@ -0,0 +1,40 @@
+
+const {fetch} = require('undici');
+const {setTimeOut} = require('node:timers/promises');
+
+const MAGIC_KEY = '25478';
+
+const retraso = setTimeOut;
+
+exports.GeneradorDeLlaveMagicaThirdParty = async (ms) => {
+
+    await retraso(ms);
+
+    const { status } = await fetch(
+        'http://localhost:3366/webhook',
+        {
+            body: JSON.stringify({ magicKey: MAGIC_KEY }),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    )
+
+    if (status !== 200) {
+
+        throw new Error('Falló la petición a la magicKey');
+    }
+}
+
+exports.fetchDataSensible = async (key) => {
+
+    await retraso(700)
+    const data = { sensitive: true }
+
+    if (key === MAGIC_KEY) {
+        return data
+    }
+
+    throw new Error('Clave inválida');
+}
